08cbe9307dd731958aa21f312be0d9d809c1b250
Pablo-Perera <pablo.perera101ralu.ulpgc.es>
2025-11-29 12:46:55 +0000

    Página de bienvenida

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+    title: 'Empresa XYZ - Innovación y Tecnología',
+    tagline: 'Soluciones digitales para tu negocio',
+    services: [
+      'Desarrollo de Software',
+      'Consultoría Tecnológica',
+      'Transformación Digital',
+      'Ciberseguridad'
+    ],
+    imagePath: '/images/empresa.png' // ruta de la imagen
+  });
 });
 
 module.exports = router;
